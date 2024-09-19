c2d21363186be248f5173276b48a6c55465ee97b
Khalil <119094864+Traineris@users.noreply.github.com>
2024-09-19 15:08:22 +0700

    Update index.jsx (notifikasi)

--- src/pages/notification/index.jsx
+++ src/pages/notification/index.jsx
@@ -1,13 +1,74 @@
-import { useEffect, useState } from 'react';
-import MobileLayout from '../../layouts/mobileLayout';
+import { useEffect, useState } from "react";
+import MobileLayout from "../../layouts/mobileLayout";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBell } from "@fortawesome/free-solid-svg-icons";
 
 const Notification = () => {
+  const [currentTime, setCurrentTime] = useState("");
 
-    return (
-        <MobileLayout title="Notification">
-            <p>Notification</p>
-        </MobileLayout>
-    )
-}
+  // Fungsi untuk mendapatkan waktu saat ini
+  useEffect(() => {
+    const updateTime = () => {
+      const now = new Date();
+      const formattedTime = `${now.getDate().toString().padStart(2, "0")}/${(now.getMonth() + 1)
+        .toString()
+        .padStart(2, "0")}/${now.getFullYear()}`;
+      setCurrentTime(formattedTime);
+    };
 
-export default Notification
+    updateTime();
+    const timer = setInterval(updateTime, 60000); // Update setiap 1 menit
+
+    // Membersihkan interval ketika komponen di-unmount
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <MobileLayout title="Notification" className="p-6 bg-gray-100 border border-gray-200 rounded-lg shadow-sm">
+
+      
+
+      
+
+      <div className="bg-white p-4 rounded-lg hover:shadow-md border border-transparent hover:text-emerald-800 hover:border-emerald-800 mb-2">
+        <div>
+          <div className="flex items-center space-x-1">
+            <FontAwesomeIcon icon={faBell} className=" transition-colors duration-300 pb-1 text-xs text-blue-800" />
+            <h5 className="text-l font-semibold mb-1">Notification</h5>
+            <p className="pb-1">•</p>
+            <p className="pb-1 text-xs">{currentTime}</p>
+          </div>
+        </div>
+        <p className=" text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. At, quae.</p>
+      </div>
+
+      <div className="bg-white p-4 rounded-lg hover:shadow-md border border-transparent hover:text-emerald-800 hover:border-emerald-800 mb-2">
+        <div>
+          <div className="flex items-center space-x-1">
+            <FontAwesomeIcon icon={faBell} className=" transition-colors duration-300 pb-1 text-xs text-blue-800" />
+            <h5 className="text-l font-semibold mb-1">Notification</h5>
+            <p className="pb-1">•</p>
+            <p className="pb-1 text-xs">{currentTime}</p>
+          </div>
+        </div>
+        <p className=" text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. At, quae.</p>
+      </div>
+      <div className="bg-white p-4 rounded-lg hover:shadow-md border border-transparent hover:text-emerald-800 hover:border-emerald-800 mb-2">
+        <div>
+          <div className="flex items-center space-x-1">
+            <FontAwesomeIcon icon={faBell} className=" transition-colors duration-300 pb-1 text-xs text-blue-800" />
+            <h5 className="text-l font-semibold mb-1">Notification</h5>
+            <p className="pb-1">•</p>
+            <p className="pb-1 text-xs">{currentTime}</p>
+          </div>
+        </div>
+        <p className=" text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. At, quae.</p>
+      </div>
+      
+      
+      
+    </MobileLayout>
+  );
+};
+
+export default Notification;
