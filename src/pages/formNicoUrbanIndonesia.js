88104f6ae8c2bb0076ecdb7760f0748fab7ad54a
pepripratama <pepripratama@gmail.com>
2024-09-21 13:33:08 +0700

    update home

--- src/pages/formNicoUrbanIndonesia.js
+++ src/pages/formNicoUrbanIndonesia.js
@@ -18,24 +18,14 @@ const Home = ({ onLogout }) => {
     }
   };
 
-  const renderStep = () => {
-    return (
-      <div className="grid grid-cols-3 gap-4">
-        <ActionButton icon={faCalendarCheck} label="Absensi" onClick={() => navigate("/absensi")} color="text-blue-500" />
-        <ActionButton icon={faBell} label="Notifikasi" onClick={() => navigate("/notification")} color="text-yellow-500" />
-        <ActionButton icon={faGrip} label="Lainnya" onClick={() => navigate("/menu")} color="text-gray-500" />
-      </div>
-    );
-  };
-
   const GetNamaDivisi = (id) => {
     let role = "";
     if (id === "1") {
-      role = "IT";
+      role = "Admin";
     } else if (id === "2") {
-      role = "GA";
+      role = "IT";
     } else if (id === "3") {
-      role = "Teksnisi";
+      role = "Teknisi";
     } else {
       role = "Divisi Tidak Diketahui";
     }
@@ -55,7 +45,7 @@ const Home = ({ onLogout }) => {
 
   return (
     <div className="flex flex-col h-screen font-sans">
-      <div style={{borderBottomLeftRadius: 20, borderBottomRightRadius: 20 }} className="flex-0 p-5 bg-green-900 border-gray-300 shadow-md">
+      <div style={{borderBottomLeftRadius: 20, borderBottomRightRadius: 20 }} className="flex-0 p-5 bg-green-900">
         <div className="flex flex-row justify-between">
           <div className="flex flex-col gap-5">
             <h2 className="text-2xl font-bold text-white">{username || "User"}</h2>
@@ -70,14 +60,18 @@ const Home = ({ onLogout }) => {
         </div>
       </div>
       <TitleDivider title="Menu"  />
-      {renderStep()}
+      <div className="grid grid-cols-3 gap-4">
+        <IconButton icon={faCalendarCheck} label="Absensi" onClick={() => navigate("/absensi")} color="text-blue-500" />
+        <IconButton icon={faBell} label="Notifikasi" onClick={() => navigate("/notification")} color="text-yellow-500" />
+        <IconButton icon={faGrip} label="Lainnya" onClick={() => navigate("/menu")} color="text-gray-500" />
+      </div>
       <TitleDivider title="Bantuan" />
       <div className="flex flex-col gap-2 px-5">
         <MenuBantuan color={"text-primary"} icon={faMessage} title="Tim IT" />
         <MenuBantuan color={"text-primary"} icon={faMessage} title="Leader" />
       </div>
       <div className="flex-1 bg-white shadow-md mb-4"></div>
-      <div className="flex justify-around p-4 bg-green-900 shadow-md">
+      <div className="flex justify-around p-4 bg-green-900 shadow-md text-white">
         <IconButton icon={faHome} label="Home" onClick={() => navigate("/home")} />
         <IconButton icon={faBell} label="Notifikasi" onClick={() => navigate("/notification")} />
         <IconButton icon={faUser} label="Profil" onClick={() => navigate("/profile")} />
@@ -87,22 +81,15 @@ const Home = ({ onLogout }) => {
 };
 
 
-const ActionButton = ({ icon, label, onClick, color }) => (
+const IconButton = ({ icon, label, onClick, color }) => (
   <button onClick={onClick} aria-label={label} className="p-4">
     <div className="flex flex-col items-center">
-      {/* sesuaikan warna tombol berdasarkan color */}
       <FontAwesomeIcon icon={icon} className={`text-2xl ${color}`} />
       <span className="mt-2 text-sm">{label}</span>
     </div>
   </button>
 );
 
-const IconButton = ({ icon, label, onClick }) => (
-  <button className="flex flex-col items-center bg-transparent cursor-pointer" onClick={onClick} aria-label={label}>
-    <FontAwesomeIcon icon={icon} className="text-2xl text-white" />
-    <span className="text-xs text-white">{label}</span>
-  </button>
-);
 
 const TitleDivider = ( {title, onClick } ) => (
   <div className="flex justify-between p-4">
