22d30b05f034f35342fe9176189e4f9a3e46a427
PRDV22 <53340059+PilipusVegas@users.noreply.github.com>
2024-10-07 10:15:56 +0700

    Update menuSidebar.js
    - menghapus menu salary

--- src/layouts/menuSidebar.js
+++ src/layouts/menuSidebar.js
@@ -1,66 +1,23 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCalendarCheck, faBell, faUser, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
-
-const ROLE_IDS = {
-  APPROVAL: "5",
-  SALARY: "4",
-};
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBell, faUser, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 
 const IconButton = ({ icon, label, onClick }) => (
-  <button
-    onClick={onClick}
-    className="flex items-center p-4 transition-colors duration-300 rounded-md hover:bg-green-700 w-full text-left"
-    aria-label={label} // Accessibility improvement
-  >
+  <button onClick={onClick} aria-label={label} className="flex items-center p-4 transition-colors duration-300 rounded-md hover:bg-green-700 w-full text-left">
     <FontAwesomeIcon icon={icon} className="text-xl mr-3" />
     <span className="text-lg font-medium">{label}</span>
   </button>
 );
 
-const MenuSidebar = ({ handleLogout, roleId }) => {
-  const navigate = useNavigate(); // Hook untuk navigasi
-  // console.log("Current roleId:", roleId); 
-
-  // Mapping roleId to menu items
-  const menuItems = {
-    [ROLE_IDS.APPROVAL]: (
-      <IconButton 
-        label="Approval" 
-        icon={faCalendarCheck} 
-        onClick={() => navigate("/data-approval")} 
-      />
-    ),
-    [ROLE_IDS.SALARY]: (
-      <IconButton 
-        label="Salary" 
-        icon={faCalendarCheck} 
-        onClick={() => navigate("/salary")} 
-      />
-    ),
-  };
-
+const MenuSidebar = ({ handleLogout }) => {
+  const navigate = useNavigate();
   return (
     <div className="w-64 bg-green-800 text-white flex flex-col p-4 shadow-md">
       <h2 className="text-2xl font-semibold mb-6 text-center">Menu</h2>
       <div className="flex flex-col gap-2">
-        {menuItems[roleId] || null} {/* Render role-specific menu item */}
-        
-        <IconButton 
-          label="Notifikasi" 
-          icon={faBell} 
-          onClick={() => navigate("/notification")} 
-        />
-        <IconButton 
-          label="Profil" 
-          icon={faUser} 
-          onClick={() => navigate("/profile")} 
-        />
-        <IconButton 
-          label="Logout" 
-          icon={faSignOutAlt} 
-          onClick={handleLogout} 
-        />
+        <IconButton icon={faBell} label="Notifikasi" onClick={() => navigate("/notification")} />
+        <IconButton icon={faUser} label="Profil" onClick={() => navigate("/profile")} />
+        <IconButton label="Logout" icon={faSignOutAlt} onClick={handleLogout} />
       </div>
     </div>
   );
